d18be5462a3adb845285bcfb6f452a8102913b1e
zhoufei <15003707121@163.com>
2017-07-31 18:00:15 +0800

    新增功能和页面

--- js/agritourism/agritourism_index.js
+++ js/agritourism/agritourism_index.js
@@ -24,6 +24,16 @@ console.log(' '+' '+' '+' '+' '+' '+' '+' '+' '+"%c$$$",'color:#0f0');
 console.log(' '+' '+' '+' '+' '+' '+' '+' '+' '+"%c$$$",'color:#0f0');
 console.log(' '+' '+' '+' '+' '+' '+' '+' '+' '+"%c$$$",'color:#0f0');
 console.log(' '+' '+' '+' '+' '+' '+' '+' '+' '+"%c$$$",'color:#0f0');
-$("#agritourismSearch").click(function() {
-	$("body")[0].style.backgroundColor = "#00FF00";
-});
+window.onload = function() {
+	$("#agritourismSearch").click(function() {
+		alert("没有匹配的内容");
+	});
+	
+    console.log(document.body.scrollTop);
+    if (document.body.scrollTop >= 328) {
+    	$(".agritourismSearch").css({"position":"fixed","top":"0"});
+    }
+	
+}
+
+
